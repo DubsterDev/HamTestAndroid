7c6786940d805d426edde4db3d980b5918e7971a
Dubster <dubster@localhost>
2026-06-08 17:29:32 -0500

    Remove unused Greeting

--- app/src/main/java/com/hazelhope/dubster/hamtest/MainActivity.kt
+++ app/src/main/java/com/hazelhope/dubster/hamtest/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HamTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    App(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +30,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App(modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     HamTestTheme {
-        Greeting("Android")
+        App()
     }
 }
