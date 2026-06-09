902dc3ecd219438036a493cd88753f6a590134b2
Dubster <dubster@localhost>
2026-06-09 11:49:55 -0500

    Add wrong/correct message

--- app/src/main/java/com/hazelhope/dubster/hamtest/MainActivity.kt
+++ app/src/main/java/com/hazelhope/dubster/hamtest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -29,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -137,10 +140,21 @@ fun Quiz(quizType: String, modifier: Modifier = Modifier) {
             correct_letter = "A"
         )
     )) }
-    var selectedAnswer by remember { mutableIntStateOf(0) }
+
     LaunchedEffect(quizType) {
         val quizRaw = context.assets.open("$quizType.json").bufferedReader().use { it.readText() }
         quiz = Json.decodeFromString<List<HamQuestion>>(quizRaw)
+    }
+
+    var currentQuestion by remember { mutableIntStateOf(0) }
+    var checked by remember { mutableStateOf(false) }
+
+    var selectedAnswer by remember { mutableIntStateOf(0) }
+
+    var isCheckedCorrect by remember { mutableStateOf(false) }
+
+    LaunchedEffect(selectedAnswer) {
+        isCheckedCorrect = selectedAnswer == quiz[currentQuestion].correct
     }
     Column(
         verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Bottom),
@@ -148,14 +162,15 @@ fun Quiz(quizType: String, modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize().padding(12.dp)
     ) {
         Text(
-            text = quiz[0].question,
+            text = quiz[currentQuestion].question,
             style = MaterialTheme.typography.headlineSmall
         )
         Column(
             verticalArrangement = Arrangement.spacedBy(12.dp),
             modifier = Modifier.selectableGroup()
         ) {
-            quiz[0].answers.forEachIndexed { index, answer ->
+            val letters = listOf("A", "B" , "C", "D")
+            quiz[currentQuestion].answers.forEachIndexed { index, answer ->
                 Row(
                     Modifier
                         .fillMaxWidth()
@@ -172,19 +187,62 @@ fun Quiz(quizType: String, modifier: Modifier = Modifier) {
                         onClick = null
                     )
                     Text(
-                        text = answer,
+                        text = "${letters[index]}. $answer",
                         style = MaterialTheme.typography.bodyLarge,
                         modifier = Modifier.padding(start = 16.dp)
                     )
                 }
             }
         }
-        Button({},
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(
-                text = "Continue"
-            )
+        if (!checked) {
+            Button({
+                checked = true
+            },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Submit"
+                )
+            }
+        } else {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
+                ) {
+                    Image(
+                        if (isCheckedCorrect) painterResource(R.drawable.outline_check_circle)
+                        else painterResource(R.drawable.outline_cancel),
+                        contentDescription = null
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(start = 12.dp)
+                    ) {
+                        Text(
+                            text = if (isCheckedCorrect) "Correct!" else "Wrong.",
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        Text(
+                            text = if (isCheckedCorrect) "That was the right answer."
+                            else "The right answer was ${quiz[currentQuestion].correct_letter}"
+                        )
+                    }
+                }
+            }
+            Button({
+
+            },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Next Question"
+                )
+            }
         }
     }
 }
