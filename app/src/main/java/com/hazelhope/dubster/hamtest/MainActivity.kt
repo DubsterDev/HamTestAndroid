2179403185627f4b705f9a6c378882fc2060c918
Dubster <dubster@localhost>
2026-06-15 15:18:19 -0500

    Add progress bar

--- app/src/main/java/com/hazelhope/dubster/hamtest/MainActivity.kt
+++ app/src/main/java/com/hazelhope/dubster/hamtest/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -382,6 +383,10 @@ fun Quiz(
                 viewModel.nextQuestion(false)
             })
         } else {
+            LinearProgressIndicator(
+                progress = { (questionPoolData.inUsePoolSize / questionPoolData.totalPoolSize).toFloat() },
+                modifier = Modifier.fillMaxWidth()
+            )
             QuestionPoolDiagnostics(questionPoolData)
             QuestionPoolQuestion(currentQuestion, {
                 viewModel.nextQuestion(it)
