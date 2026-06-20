83729a0cb4af92bf84fd9b6640403217710de666
Dubster <dubster@localhost>
2026-06-19 19:46:42 -0500

    Add advance buttons

--- app/src/main/java/com/hazelhope/dubster/hamtest/PracticeTest.kt
+++ app/src/main/java/com/hazelhope/dubster/hamtest/PracticeTest.kt
@@ -2,14 +2,22 @@ package com.hazelhope.dubster.hamtest
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hazelhope.dubster.hamtest.ui.theme.HamTestTheme
@@ -42,8 +50,8 @@ fun PracticeTest(
     val currentQuestion by viewModel.currentQuestion.collectAsStateWithLifecycle()
 
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom
+        modifier = modifier.fillMaxSize().padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Bottom)
     ) {
         QuestionPoolJustQuestion(
             currentQuestion = currentQuestion.hamQuestion,
@@ -53,6 +61,34 @@ fun PracticeTest(
             },
             enabled = true
         )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(
+                onClick = {
+                    viewModel.changeQuestion("previous")
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(
+                    painterResource(R.drawable.outline_arrow_back),
+                    contentDescription = "Previous question"
+                )
+            }
+            IconButton(
+                onClick = {
+                    viewModel.changeQuestion("next")
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(
+                    painterResource(R.drawable.outline_arrow_forward),
+                    contentDescription = "Next question"
+                )
+            }
+        }
     }
 }
 
