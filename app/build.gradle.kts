89a4058ca3f7319fd5f9633a142ebfa32d183cdf
DubsterDev <317384959+DubsterDev@users.noreply.github.com>
2026-08-15 15:16:58 -0500

    Add fastlane metadata, update the version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.hazelhope.dubster.hamtest"
         minSdk = 25
         targetSdk = 37
-        versionCode = 114
-        versionName = "1.1.4"
+        versionCode = 115
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
