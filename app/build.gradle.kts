5154d4ad93d9739b6f1c271e41c130957ace6137
DubsterDev <317384959+DubsterDev@users.noreply.github.com>
2026-08-22 10:43:16 -0500

    Release v1.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.hazelhope.dubster.hamtest"
         minSdk = 25
         targetSdk = 37
-        versionCode = 118
-        versionName = "1.1.8"
+        versionCode = 119
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
